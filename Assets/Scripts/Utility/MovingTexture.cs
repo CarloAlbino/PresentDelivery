c81a121eb08e3285d34eceb039a2566f01e52fe8
CarloAlbino <carloalbino@gmail.com>
2016-12-23 22:33:57 -0500

    Fancying up the set

---
+++ Assets/Scripts/Utility/MovingTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTexture : MonoBehaviour {
+
+    private Material m_material;
+    public float m_speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        m_material = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = m_material.mainTextureOffset;
+        offset.x -= Time.deltaTime * m_speed;
+        m_material.mainTextureOffset = offset;
+	}
+}
